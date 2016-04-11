340cea8936a0e06802ea0f9c5e98d51d5243ba70
Claire Savage <clairejsavage@gmail.com>
2016-04-10 20:25:23 -0700

    copy added and styling updated

--- main.js
+++ main.js
@@ -1,26 +1,7 @@
 $(document).ready(function(){
-//   $('.parallax').parallax();
-//   $('.scrollspy').scrollSpy();
-// });
+ });
 
   $('.parallax-window').parallax({imageSrc: 'assets/image.jpg'});
-
-  $('.bwWrapper').BlackAndWhite({
-        hoverEffect : true, // default true
-        // set the path to BnWWorker.js for a superfast implementation
-        webworkerPath : false,
-        // to invert the hover effect
-        invertHoverEffect: false,
-        // this option works only on the modern browsers ( on IE lower than 9 it remains always 1)
-        intensity:1,
-        speed: { //this property could also be just speed: value for both fadeIn and fadeOut
-            fadeIn: 200, // 200ms for fadeIn animations
-            fadeOut: 800 // 800ms for fadeOut animations
-        },
-        onImageReady:function(img) {
-            // this callback gets executed anytime an image is converted
-        }
-    });
 
   $('nav a').click(function () {
    var $href = $(this).attr('href');
@@ -29,4 +10,4 @@ $(document).ready(function(){
    }, 500);
    return false;
   });
-)};
+
