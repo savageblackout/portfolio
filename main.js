8772bbb5d4878db5a2af36c26a5eb69ba86e0d22
Claire Savage <clairejsavage@gmail.com>
2016-04-14 20:16:15 -0700

    brand-logo change function

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
  });
 
-  $('.parallax-window').parallax({imageSrc: 'assets/image.jpg'});
+  $('#name').hover(function() {
+    $main_text=$(this).text();
+    $(this).text("CLAIRE SAVAGE");
+  }, function() {
+    $(this).text($main_text);
+
+  });
+
+  // $('#name').mouseover(function() {
+  //   $('#name').addClass('hidden');
+  //   $('#real').removeClass('hidden');
+  // });
 
   $('nav a').click(function () {
    var $href = $(this).attr('href');
    $('body').stop().animate({
      scrollTop: $($href).offset().top
-   }, 500);
+   }, 1000);
    return false;
   });
+
+
 
